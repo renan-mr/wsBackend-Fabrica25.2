c5488bee07e7c0096be0a5a097e949f6a9df1467
Renan <renanmarinhodossantos9@gmail.com>
2025-09-13 17:45:15 -0300

    :arrow_up: Adicionado suporte ao PostgreSQL e pequena melhoria no serializers.py

--- Projeto_dog/Projeto_dog/settings.py
+++ Projeto_dog/Projeto_dog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Projeto_dog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'api_dog_db',          
+        'USER': 'postgres',             
+        'PASSWORD': '0000', 
+        'HOST': 'localhost',            
+        'PORT': '5432',                 
     }
 }
 
--- Projeto_dog/api_dog/serializers.py
+++ Projeto_dog/api_dog/serializers.py
@@ -9,10 +9,14 @@ class PessoaSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'data_criacao']
 
 class CachorroFavoritoSerializer(serializers.ModelSerializer):
-    
+    pessoa = serializers.StringRelatedField(read_only=True)     #transforma o id em "pessoa: nome", em vez de pessoa: id como anteriormente.
+                                                                 # pequena melhoria na exibição dos dados do JSON
+    pessoa_id = serializers.PrimaryKeyRelatedField(                  # pequena melhoria na exibição dos dados do JSON
+        queryset=Pessoa.objects.all(), source='pessoa', write_only=True  # recebe o id de pessoa.
+    )
 
     class Meta:
         model = CachorroFavorito
 
-        fields = ['id', 'id_raca', 'nome', 'descricao', 'notas', 'criado_em', 'pessoa']
+        fields = ['id', 'id_raca', 'nome', 'descricao', 'notas', 'criado_em', 'pessoa', 'pessoa_id']
         read_only_fields = ['id', 'criado_em']
